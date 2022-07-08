8490470f3bae731806dff93b0602015914d84f92
github_krishna_4257 <jayakrishna4257@gmail.com>
2022-07-08 15:09:15 +0530

    My personal portfolio website using HTMl CSS JS

---
+++ Resume.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('#profile_ripple').ripples({
+        resolution: 512,
+        dropRadius:10
+    })
+
+    const bars = document.querySelectorAll('.progress_bar');
+
+    bars.forEach(function(bar){
+        let percentage = bar.dataset.percent;
+        let per = bar.children[0];
+        per.innerText = percentage + '%';
+        bar.style.width = percentage + '%';
+        console.log(percentage);
+    })
+})
